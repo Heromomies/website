b2b1057837fcfa11ed301c7d66f3b1bd2ec81711
Loann <courcoul66@gmail.com>
2021-03-09 15:53:01 +0100

    Test materialize

---
+++ js/game.js
@@ -0,0 +1,13 @@
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  
+  M.toast({html: 'I am a toast!'})
